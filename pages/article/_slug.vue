a81be715da6e255ff46db239e46d979c9f58d153
CameronWhitworth <cameronwhitworth@hotmail.com>
2021-11-06 16:10:19 +0000

    added details for blog views

--- pages/article/_slug.vue
+++ pages/article/_slug.vue
@@ -1,36 +1,83 @@
 <template>
   <v-app>
-    <v-content class="text-center">
-      <v-row
+    <v-content>
+      <!-- <v-row
         justify="center"
         v-for="item in data.blogCollection.items"
         v-bind:key="item.items"
       >
         <div>
-            <v-hover v-slot:default="{ hover }">
-                <v-card width="100vh" class="ma-0" :elevation="hover ? 12 : 2" :to="{ name: 'article-slug', params: { slug: item.slug } }">
-                    <v-list-item>
-                    <v-img
-                        class="ma-2"
-                        :src="item.preview.url"
-                        :aspect-ratio="16 / 9"
-                        max-width="300px"
-                    ></v-img>
-                    <v-list-item-content>
-                        <div class="overline mb-4">TYPE OF ARTICAL</div>
-                        <v-list-item-title class="headline mb-1 blue--text">
-                        {{ item.title }}
-                        </v-list-item-title>
-                        <v-list-item-title class="title mb-1">{{
-                        item.textPreview
-                        }}</v-list-item-title>
-                        <div>{{ item.textPreview }}</div>
-                    </v-list-item-content>
-                    </v-list-item>
-                </v-card>
-            </v-hover>
+          <v-hover v-slot:default="{ hover }">
+            <v-card
+              width="100vh"
+              class="ma-0"
+              :elevation="hover ? 12 : 2"
+              :to="{ name: 'article-slug', params: { slug: item.slug } }"
+            >
+              <v-list-item>
+                <v-img
+                  class="ma-2"
+                  :src="item.preview.url"
+                  :aspect-ratio="16 / 9"
+                  max-width="300px"
+                ></v-img>
+                <v-list-item-content>
+                  <div class="overline mb-4">TYPE OF ARTICAL</div>
+                  <v-list-item-title class="headline mb-1 blue--text">
+                    {{ item.title }}
+                  </v-list-item-title>
+                  <v-list-item-title class="title mb-1">{{
+                    item.textPreview
+                  }}</v-list-item-title>
+                  <div>{{ item.textPreview }}</div>
+                </v-list-item-content>
+              </v-list-item>
+            </v-card>
+          </v-hover>
         </div>
-      </v-row>
+      </v-row> -->
+      <section id="post">
+        <v-container
+          v-for="item in data.blogCollection.items"
+          v-bind:key="item.items"
+        >
+          <v-responsive class="mx-auto mb-12" width="720">
+            <h2
+              class="display-3 font-weight-bold mb-3 text-center text-uppercase"
+            >
+              {{ item.title }}
+            </h2>
+            
+            <h5 class="font-weight-bold mb-3 text-left text--secondary">
+              {{ item.author }}, {{ item.date }}
+            </h5>
+            <!-- <h5 class="font-weight-bold mb-3 text-left text--secondary">
+              {{ item.author }}
+            </h5> -->
+
+            <v-divider></v-divider>
+            
+            <v-img
+              class="my-1"
+              :src="item.preview.url"
+              :aspect-ratio="16 / 9"
+            ></v-img>
+            <h5 class="font-weight-bold mb-3 text-left text--secondary">
+              {{ item.preview.title }}
+            </h5>
+
+            <v-divider class="mb-1"></v-divider>
+
+            <v-divider></v-divider>
+            <v-responsive
+              class="mx-auto title font-weight-light mb-8"
+              max-width="720"
+            >
+              {{ item.textBody }}
+            </v-responsive>
+          </v-responsive>
+        </v-container>
+      </section>
     </v-content>
   </v-app>
 </template>
